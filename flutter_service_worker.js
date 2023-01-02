a7f86d42d722ad652608f6947a8275d00a9839e4
fabbo-repo <47057180+fabbo-repo@users.noreply.github.com>
2023-01-02 13:08:57 +0000

    Update web-page to output generated at ab40a99

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -553,8 +553,8 @@ const RESOURCES = {
 "assets/assets/images/profile.jpg": "5433101042e937ca714ca4092af3ab59",
 "assets/assets/images/background.jpg": "0a2b51f8033ec1bcf6fbd2a56ab010fa",
 "assets/NOTICES": "1e16e1f4a455fe5a66814a7b4c9db453",
-"index.html": "202b8a2996ec7c20a199a03a230e5473",
-"/": "202b8a2996ec7c20a199a03a230e5473",
+"index.html": "55f38d46a8f83e4ffc4a85208c050bed",
+"/": "55f38d46a8f83e4ffc4a85208c050bed",
 "favicon.png": "e7883eba65492012a894cdad6b5385cd",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
